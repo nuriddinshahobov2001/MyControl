bc6e08e010f967a606f1a13cdc7f80eb23a1025e
Khusrav-Muhammadi <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2023-09-26 19:37:24 +0500

    hjb

--- app/Http/Controllers/API/ClientController.php
+++ app/Http/Controllers/API/ClientController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\ClientResource;
 use App\Http\Services\ClientService;
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use function Laravel\Prompts\alert;
 
 class ClientController extends Controller
@@ -19,7 +20,17 @@ class ClientController extends Controller
     }
 
     public function index() {
-        $clients = Client::get();
+
+        $clients = DB::table('clients')
+            ->select('clients.id', 'clients.fio', 'clients.limit', 'clients.amount', 'clients.address',
+                'clients.description', 'clients.phone', 'clients.balance',
+                DB::raw('SUM(CASE WHEN credit__debit__histories.type = "debit" THEN credit__debit__histories.summa ELSE 0 END) -
+                       SUM(CASE WHEN credit__debit__histories.type = "credit" THEN credit__debit__histories.summa ELSE 0 END) as debt'))
+            ->leftJoin('credit__debit__histories', 'clients.id', '=', 'credit__debit__histories.client_id')
+            ->groupBy('clients.id', 'clients.fio', 'clients.limit', 'clients.amount', 'clients.address',
+                'clients.description', 'clients.phone', 'clients.balance')
+            ->get();
+
         return response([
             'message' => true,
             'clients' => $clients
