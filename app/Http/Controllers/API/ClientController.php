4f7810dd959914f503175e2f23ecc1f77a43709d
Nuriddin Shahobov <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2023-10-31 10:56:59 +0500

    rth

--- app/Http/Controllers/API/ClientController.php
+++ app/Http/Controllers/API/ClientController.php
@@ -14,6 +14,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use function Symfony\Component\Translation\t;
 
 class ClientController extends Controller
 {
@@ -25,15 +26,7 @@ class ClientController extends Controller
 
     public function index() {
 
-        $clients = DB::table('clients')
-            ->select('clients.id', 'clients.fio', 'clients.limit', 'clients.amount', 'clients.address',
-                'clients.description', 'clients.phone', 'clients.balance',
-                DB::raw('SUM(CASE WHEN credit__debit__histories.type = "debit" THEN credit__debit__histories.summa ELSE 0 END) -
-                       SUM(CASE WHEN credit__debit__histories.type = "credit" THEN credit__debit__histories.summa ELSE 0 END) as debt'))
-            ->leftJoin('credit__debit__histories', 'clients.id', '=', 'credit__debit__histories.client_id')
-            ->groupBy('clients.id', 'clients.fio', 'clients.limit', 'clients.amount', 'clients.address',
-                'clients.description', 'clients.phone', 'clients.balance')
-            ->get();
+        $clients = $this->clientService->index();
 
         return response([
             'message' => true,
--- app/Http/Services/ClientService.php
+++ app/Http/Services/ClientService.php
@@ -10,7 +10,15 @@ use Illuminate\Support\Facades\DB;
 class ClientService {
 
     public function index() {
-        return User::role('client')->get();
+        return DB::table('clients')
+            ->select('clients.id', 'clients.fio', 'clients.limit', 'clients.amount', 'clients.address',
+                'clients.description', 'clients.phone', 'clients.balance',
+                DB::raw('SUM(CASE WHEN credit__debit__histories.type = "debit" THEN credit__debit__histories.summa ELSE 0 END) -
+                       SUM(CASE WHEN credit__debit__histories.type = "credit" THEN credit__debit__histories.summa ELSE 0 END) as debt'))
+            ->leftJoin('credit__debit__histories', 'clients.id', '=', 'credit__debit__histories.client_id')
+            ->groupBy('clients.id', 'clients.fio', 'clients.limit', 'clients.amount', 'clients.address',
+                'clients.description', 'clients.phone', 'clients.balance')
+            ->get();
     }
 
     public function store($data) {
