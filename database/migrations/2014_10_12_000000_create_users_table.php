8ff2002cbe670e7d51d82a309e6b334b470cd574
Nuriddin Shahobov <117286325+Khusrav-Muhammadi@users.noreply.github.com>
2023-11-03 11:53:35 +0500

    few

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('phone')->unique();
             $table->string('password');
             $table->string('img')->nullable();
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
